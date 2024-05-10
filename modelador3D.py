9442f39a1895ae12846c0fadc6d408dd59c4494a
Maria Eduarda Crema Carlos <65905315+MaduhCrema@users.noreply.github.com>
2024-05-10 15:15:24 -0300

    Update modelador3D.py
    Dsenha varios objetos 2D, e esta armazenando de forma correta a lista de arestas na lista de objetos criados e quando passa como parâmetro pro drawSlices(que ainda nao funciona)

--- modelador3D.py
+++ modelador3D.py
@@ -1,19 +1,24 @@
 import pygame
-import numpy as pd
 import math
+import time
 # Inicialização do Pygame
 pygame.init()
 
 # Configurações da tela, lembrando que o Y é invertido menor em cima e maior embaixo
 largura, altura = 800, 600
-tela = pygame.display.set_mode((largura, altura))
+tela_principal = pygame.display.set_mode((largura, altura))
+tela_slice = pygame.display.set_mode((largura, altura))
 
 # Listas
 objetos = []
 vertice_aux = []
 vertices_objeto = []
 arestas_objeto = []
-num_total_vertices = 0
+vertices_objt_fatiado = []
+arestas_objet_fatiado = []
+objetos_fatiados = []
+
+# Variaveis
 num_fatias_desejado = 10
 
 # Estrutura do objeto
@@ -36,42 +41,38 @@ class Aresta:
         return f"Aresta de {self.vertice_inicial} para {self.vertice_final}"
 
 def createVertice(x, y, z):
-    # O vertice_aux é para auxiliar a criação das arestas, sempre pegando o vértice final da outra aresta para ser o inicial da próxima
     vertices_objeto.append(Vertice(x, y, z))
-    num_total_vertices =+ 1
-
-    print("VÉRTICES:")
-    for vertice in vertices_objeto:
-        print(vertice)
-
     vertice_aux.append(Vertice(x, y, z))
-    # Se houver dois vértices, cria uma aresta
     if len(vertice_aux) == 2:
         aresta_aux = Aresta(vertice_aux[0], vertice_aux[1])
         arestas_objeto.append(aresta_aux)
-        print("ARESTAS")
-        for aresta in arestas_objeto:
-            print(aresta)
-        # Considera que o vértice 2 vai para a posição 0 e a posição 1 é excluída para receber o próximo vértice
+        # Adiciona à última lista de arestas de objetos, ou cria um novo objeto se a lista de objetos estiver vazia
+        if objetos:
+            objetos[-1][1].append(aresta_aux)
+        else:
+            objetos.append((vertices_objeto.copy(), arestas_objeto.copy()))
         vertice_aux[0] = vertice_aux[1]
         vertice_aux.pop(1)
 
 #função que representa as vertice do objeto
-def drawVertices():
+def drawVertices(tela):
     for vertice in vertices_objeto:
         pygame.draw.circle(tela, (0, 0, 0), (vertice.x, vertice.y), 3)  # Desenha um círculo preto
 
 #função que representa as arestas do objeto
-def drawEdges():
-    for i in range(len(vertices_objeto) - 1):
-        pygame.draw.line(tela, (0, 0, 0), (vertices_objeto[i].x, vertices_objeto[i].y),
-                         (vertices_objeto[i + 1].x, vertices_objeto[i + 1].y), 1)
+def drawEdges(tela, lista_de_arestas):
+    for aresta_objeto in lista_de_arestas:
+        ponto1 = (aresta_objeto.vertice_inicial.x, aresta_objeto.vertice_inicial.y)
+        ponto2 = (aresta_objeto.vertice_final.x, aresta_objeto.vertice_final.y)
+        pygame.draw.line(tela, (0, 0, 0), ponto1, ponto2, 1)
         
 def rotateObject(objeto, theta):
-    for vertice in objeto[0]:  # Rotaciona cada vértice
-        vertice.x, vertice.y, _ = rotatePoint(vertice.x, vertice.y, vertice.z, theta)
-
+    print("Rotação do objeto:")
+    for vertice in objeto:  # Rotaciona cada vértice
+        createVertice(rotatePoint(vertice.x, vertice.y, vertice.z, theta))
+        
 def rotatePoint(x, y, z, theta):
+    print(x,y,z,theta)
     cos_theta = math.cos(theta)
     sin_theta = math.sin(theta)
     new_x = x * cos_theta - y * sin_theta
@@ -79,16 +80,28 @@ def rotatePoint(x, y, z, theta):
     return new_x, new_y, z
 
 def drawSlices(objeto, num_fatias):
-    for i in range(num_fatias):
+    print("drawing slices")
+    print("vertices:")
+    for vertice in objeto[0]:
+        print(vertice)
+    print("arestas:")
+    for aresta in objeto[1]:
+        print(aresta)
+    for i in range(num_fatias):            
         theta = math.radians(360 / num_fatias)  # Calcule o ângulo de rotação para cada fatia
         rotateObject(objeto, theta)  # Rotacione o objeto
-        drawEdges()  # Desenhe as arestas do objeto após cada rotação
-
         
+
+
+def limpaListas():
+    vertices_objeto.clear()
+    arestas_objeto.clear() 
+    vertice_aux.clear() 
+
 #Função que pega os cliques
 def handleCanvasClick(event):
     if event.button == 1:  # Botão esquerdo
-        rect = tela.get_rect()
+        rect = tela_principal.get_rect()
         x = event.pos[0] - rect.left
         y = event.pos[1] - rect.top
 
@@ -98,14 +111,14 @@ def handleCanvasClick(event):
         createVertice(x, y, z)
 
     if event.button == 3:  # Botão direito
-        aresta_aux = Aresta (vertices_objeto[num_total_vertices-1], vertices_objeto[0])
+        #coloca a aresta final para fechar o objeto
+        aresta_aux = Aresta (vertices_objeto[-1], vertices_objeto[0])
         arestas_objeto.append(aresta_aux)
+    
+        #coloca as infos na lista de objetos criados
         objeto = vertices_objeto.copy(), arestas_objeto.copy()
         objetos.append(objeto)
-        #limpa as listas para o proximo objeto
-      #  vertices_objeto.clear()
-       # arestas_objeto.clear()
-
+        
         print("Objeto criado")
         for vertices, arestas in objetos:
             print("Vertices:")
@@ -116,8 +129,10 @@ def handleCanvasClick(event):
                 print(aresta)
 
         drawSlices(objeto, num_fatias_desejado)
-        vertices_objeto.clear()
-        arestas_objeto.clear()
+        time.sleep(2)
+        limpaListas()
+        
+        
 
 ##############Rodando as funções#################
 # Cor de fundo (branco)
@@ -132,29 +147,23 @@ while rodando:
         elif evento.type == pygame.MOUSEBUTTONDOWN:
             handleCanvasClick(evento)
 
-    # Preenche a tela com o fundo branco
-    tela.fill(cor_branca)
+    tela_principal.fill(cor_branca)
+    tela_slice.fill(cor_branca)
 
-    # Desenha os pontos pretos para criar o fundo pontilhado
-    for x in range(0, largura, 17):  # Desenha pontos horizontalmente com um intervalo de 10 pixels
-        for y in range(0, altura, 17):  # Desenha pontos verticalmente com um intervalo de 10 pixels
-            pygame.draw.circle(tela, (0, 0, 0), (x, y), 1)  # Desenha um ponto preto de tamanho 1 em (x, y)
+    for x in range(0, largura, 17):  
+        for y in range(0, altura, 17):  
+            pygame.draw.circle(tela_principal, (0, 0, 0), (x, y), 1)  
 
-    # Desenha os vértices
-    drawVertices()
+    for x in range(0, largura, 17):  
+        for y in range(0, altura, 17):  
+            pygame.draw.circle(tela_slice, (0, 0, 0), (x, y), 1)  
+    #desenha as vertices e depois apaga
+    drawVertices(tela_principal)
 
-    # Desenha as arestas
-    drawEdges()
+    # Desenhe as arestas de cada objeto separadamente
+    for vertices, arestas in objetos:
+        drawEdges(tela_principal, arestas)
 
-    # Atualize os elementos do jogo aqui
-    pygame.display.flip()  # Atualize a tela
+    pygame.display.flip()
 
 pygame.quit()
-
-## dividir a figura em fatias
-##transformar esse objeto com o numero determiado de fatias: O número de fatias usadas na revolução foi igual a 30 em torno do objeto, ou
-#seja, a cada 360º/30 = 12º cria-se uma nova seção/fatia no objeto.
-##a técnica wireframe com ocultação de superfícies(pintar a face mais longe para a mais perto(pintor) apenas nas faces 
-#que estão visiveis(algoritmo de visibilidade pelo calculo da normal))
-## O numero de fatia influencia na quantidade de arestas sobrepostas, tendo mais intersecções dificulta no cálculo da ocultação de fatia
-## o numero de fatias influencia no algoritmo de pinto de forma que se houver mais fatias haverá mais sobreposições entre as faces na hora de pintar
